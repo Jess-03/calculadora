2d19a0ee37c2d9fc6120ca98d9bc3395e0d844ff
Jessica Garcia <jessicavgco@gmail.com>
2023-01-15 02:02:59 -0600

    feat: prueba de js

---
+++ index.js
@@ -0,0 +1,8 @@
+//Resultado
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+
+
+//Botones
+const botonesNumero = document.querySelectorAll('.numero');
+const botonesOperador= document.querySelectorAll('.operador');
